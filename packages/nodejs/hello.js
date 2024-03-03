af2c99d7a61145c8d9d4dd4b2526e1dc3f1a3946
Michele Sciabarra <michele@nuvolaris.io>
2024-03-03 22:23:58 +0000

    package.json updated

--- packages/nodejs/hello.js
+++ packages/nodejs/hello.js
@@ -1,6 +1,8 @@
 //--web true
 //--docker ghcr.io/nuvolaris/runtime-nodejs-v21:3.1.0-mastrogpt.2402201748
 
+const redis = require("redis")
+
 function main(args) {
     let name = args.name || "world"
     return { body: `Hello, ${name}` }
