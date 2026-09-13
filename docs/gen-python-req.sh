ca5c44e8bc4f3dcf879d16eec8fd88549236d1b2
Michele Sciabarra <michele@sciabarra.com>
2026-09-13 14:28:25 +0200

    reworked runtimes for license compliance

--- docs/gen-python-req.sh
+++ docs/gen-python-req.sh
@@ -14,26 +14,35 @@
 # KIND, either express or implied.  See the License for the
 # specific language governing permissions and limitations
 # under the License.
-requests
-pyyaml
-httplib2
-python-dateutil
-boto3
-minio
-pg8000
-redis
-pymongo
-kafka_python
-bcrypt
+# default packages available across all runtimes
 
-beautifulsoup4
-feedparser
-joblib
-plotly
 
-openai
-ollama
-langchain
-nltk
-langdetect
+# this is not a script but a sequence of commands to be run manually
+
+DIR=sys
+DIR=v3.11
+DIR=v3.12
+DIR=v3.13
+cd "$(dirname $0)"
+BASE=$(pwd)
+for DIR in sys v3.11 v3.12 v3.13
+do
+    # pick your dir
+    cd $BASE/../runtime/python/$DIR
+
+docker run -i -v $PWD:/mnt $(awk '/FROM python/{ print $2}' Dockerfile) bash <<EOF
+cd /mnt
+rm requirements.txt
+
+# pip
+pip install pip-tools
+pip-compile requirements.in
+{ head -n 16 requirements.in; cat requirements.txt; } > requirements.new && mv requirements.new requirements.txt
+EOF
+
+done
+
+# uv
+#head -n 16 requirements.in >requirements.txt
+#uv pip compile requirements.in  >>requirements.txt
 
